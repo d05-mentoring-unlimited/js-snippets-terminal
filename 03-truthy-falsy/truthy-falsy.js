2c688c654c62dbf49a3de69eaf7c6841e20568cf
losrobbos <50549469+losrobbos@users.noreply.github.com>
2023-09-01 13:37:42 +0200

    Update truthy-falsy.js

--- 03-truthy-falsy/truthy-falsy.js
+++ 03-truthy-falsy/truthy-falsy.js
@@ -1,11 +1,5 @@
 const benjaminIsAdmin = false
 
-let meineVar = NaN // undefined
-console.log(meineVar)
-
-// empty value => meineVaring => "", number: 0, 
-
-// "hallo" == true
 if(benjaminIsAdmin) {
   console.log("meineVar ist truthy");
   // redirect me to admin page
@@ -15,8 +9,8 @@ else {
   // show error message that user no rights to pass
 }
 
-
-// console.log(meineVar === true);
+const meineVar = "hallo"
+// console.log(meineVar === true); // false!
 // const comparison = meineVar == true; // comparison => always returns us back a BOOLEAN
 // console.log(comparison);
 
