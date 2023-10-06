c11df4d4d1e14f6eda8993fd8bafbf3738240854
losrobbos <losrobbos@web.de>
2023-10-06 18:56:56 +0200

    Updated comments

--- 08-higher-order-functions/find.js
+++ 08-higher-order-functions/find.js
@@ -3,10 +3,11 @@ const pizzas = [
   { name: "Hawaii", price: 5.99 },
   { name: "Salami", price: 6.99 },
   { name: "Döner", price: 8.99 },
-]
+];
 
+// find just gives us the FIRST found item
+// and it will give us the found element back, not an array like filter!
 const result = pizzas.find((pizza) => {
-    return pizza.price > 5
-  })
-console.log(result)
-
+  return pizza.price > 5;
+});
+console.log(result);
